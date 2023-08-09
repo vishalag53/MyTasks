41c2776a15d421e358e7d9e9261f6c2b2041f853
vishalag53 <vishal.agrawal1207@gmail.com>
2023-08-09 19:11:06 +0530

    Alert Dialog box added for Back Button

--- app/src/main/java/com/vishalag53/mytasks/login/LogInStartFragment.kt
+++ app/src/main/java/com/vishalag53/mytasks/login/LogInStartFragment.kt
@@ -1,22 +1,53 @@
 package com.vishalag53.mytasks.login
 
+import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toast.makeText
+import androidx.fragment.app.Fragment
 import com.vishalag53.mytasks.R
+import com.vishalag53.mytasks.databinding.FragmentLogInStartBinding
 
 
 class LogInStartFragment : Fragment() {
 
+    private lateinit var binding: FragmentLogInStartBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in_start, container, false)
+        binding = FragmentLogInStartBinding.inflate(inflater)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.backBtn.setOnClickListener {
+            backBtnAction()
+        }
+    }
+
+    private fun backBtnAction() {
+        val alertDialogBuilder = AlertDialog.Builder(requireContext())
+            .setTitle("Not Sign In")
+            .setMessage("Are you sure, don't want to log in or sign in")
+            .setPositiveButton("YES"){ dialog, _ ->
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancle"){ dialog, _ ->
+                dialog.dismiss()
+            }
+            .setCancelable(false)
+
+        alertDialogBuilder.create().show()
     }
 
 }
