b79776402ed5f596e3a46e387d6068bac85f2905
vishalag53 <vishal.agrawal1207@gmail.com>
2023-08-09 16:52:18 +0530

    Add Log In Activity

---
+++ app/src/main/java/com/vishalag53/mytasks/login/LogInActivity.kt
@@ -0,0 +1,12 @@
+package com.vishalag53.mytasks.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vishalag53.mytasks.R
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
